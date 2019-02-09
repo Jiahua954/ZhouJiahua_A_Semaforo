37358bf2a3f029a7d11a02a6eaf7a50a2f92e1d0
JiahuaZhou <jiahua.zhou@barsanti.edu.it>
2019-02-09 08:50:46 +0100

    numeri-->stringhe

--- SEMAFORO_AUSTRIACO/SEMAFORO_AUSTRIACO.ino
+++ SEMAFORO_AUSTRIACO/SEMAFORO_AUSTRIACO.ino
@@ -4,6 +4,9 @@ int VerdeSX= 11;
 int RossoDX = 10;
 int GialloDX = 9;
 int VerdeDX = 8;
+int Lamp1 = 500;
+int Lamp2 = 1000;
+int Lamp3 = 5000;
 
 
 void setup() {
@@ -18,69 +21,69 @@ pinMode(VerdeDX, OUTPUT);
 void lampeggia1() 
 {
   digitalWrite(VerdeSX, HIGH);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeSX, LOW);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeSX, HIGH);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeSX, LOW);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeSX, HIGH);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeSX, LOW);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeSX, HIGH);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeSX, LOW);
-  delay(500);
+  delay(Lamp1);
 }
 void lampeggia2()
 {
   digitalWrite(VerdeDX, HIGH);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeDX, LOW);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeDX, HIGH);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeDX, LOW);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeDX, HIGH);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeDX, LOW);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeDX, HIGH);
-  delay(500);
+  delay(Lamp1);
   digitalWrite(VerdeDX, LOW);
-  delay(500);
+  delay(Lamp1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 digitalWrite(RossoSX, HIGH);
 digitalWrite(VerdeDX, HIGH);
-delay(5000);
+delay(Lamp3);
 digitalWrite(GialloSX, HIGH);
 lampeggia2();
 digitalWrite(GialloSX, HIGH);
 digitalWrite(GialloDX, HIGH);
 digitalWrite(RossoSX, LOW);
-delay(1000);
+delay(Lamp2);
 digitalWrite(VerdeSX, HIGH);
 digitalWrite(GialloSX, LOW);
 digitalWrite(RossoDX, HIGH);
 digitalWrite(GialloDX, LOW);
-delay(5000);
+delay(Lamp3);
 digitalWrite(GialloDX, HIGH);
 lampeggia1();
 digitalWrite(RossoDX, LOW);
 digitalWrite(GialloDX, HIGH);
 digitalWrite(GialloSX, HIGH);
-delay(1000);
+delay(Lamp2);
 digitalWrite(VerdeDX, HIGH);
 digitalWrite(RossoSX, HIGH);
 digitalWrite(GialloSX, LOW);
 digitalWrite(GialloDX, LOW);
-delay(5000);
+delay(Lamp3);
 
 
 }
